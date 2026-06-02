204dadfd4887f22a65d871d434bfb25ad818022b
CamilaGonzalez88 <gonzalez.salazar.c92@gmail.com>
2026-06-02 13:20:40 -0400

    commit inicial

---
+++ src/Operadores.java
@@ -0,0 +1,32 @@
+public class Operadores {
+    public static void main(String[] args){
+       // Aritmeticos
+        System.out.println(3+2);
+        System.out.println(3-2);
+        System.out.println(3*2);
+        System.out.println(3/2);
+        System.out.println(3%2);
+
+     // Operadores de asignación
+        int num1 = 35;
+        num1 += 3; // num1 = num1 +3
+        num1 *= 4; // num1 = num1 *4
+        num1 /= 2; // num1 = num1 /2
+        num1 %= 2; // num1 = num1 %2
+        System.out.println(num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
